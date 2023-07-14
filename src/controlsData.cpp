9e5c76488f6ec6f55737cd60ca30055bddfd0129
Bodden007 <denis-bodrov@mail.ru>
2023-07-15 00:36:07 +0500

    Final

--- src/controlsData.cpp
+++ src/controlsData.cpp
@@ -5,9 +5,6 @@ void controlsData::controls()
     std::string data;
     dataStream dataStr;
     dataStorage dataStor{N};
-    dataTime dataT;
-    std::string timeDat = dataT.timeSyst();
-    std::cout << timeDat << std::endl;
 
     while (request)
     {
--- src/includes/dataStorage.hpp
+++ src/includes/dataStorage.hpp
@@ -1,15 +1,21 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include "../includes/dataTime.hpp"
 
 class dataStorage
 {
 private:
-    std::vector<std::string> bulk;
     int N;
+    int step = 1;
+    bool dynamicBlock = false;
+    bool staticBlock = true;
 
 public:
-    std::string dataBulk(std::string&);
+    std::vector<std::string> bulk;
+    std::string dataBulk(std::string &);
+    std::string outBalk(std::vector<std::string> &);
     dataStorage(int p_N)
     {
         N = p_N;
